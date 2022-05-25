fe20e900464ca816f08ccc6bd8b3ed4aa8755455
shima <shima.alam1992@gmail.com>
2022-05-25 02:05:13 -0500

    test2

---
+++ src/main/java/com/nexttechitc/Git_hub/App.java
@@ -0,0 +1,13 @@
+package com.nexttechitc.Git_hub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/practicegit/gitclass.java
@@ -0,0 +1,10 @@
+package practicegit;
+
+public class gitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
